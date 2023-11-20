b98a5763c2feeb3768fab7b57c61a2996ce81f7d
Tushar Aher <119068251+tusharaher20@users.noreply.github.com>
2023-11-20 03:57:03 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,17 @@
 "use strict" // this syntax tell javascript to treat all the further code as newer version
+
+
+//There are various datatypes in javascript
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false 
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
