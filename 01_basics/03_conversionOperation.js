c865705b6e8f7b98127f5428386ce5c79f4ab8ac
Tushar Aher <119068251+tusharaher20@users.noreply.github.com>
2023-11-27 04:32:59 +0000

    datatypes conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,15 @@
-let score = "33";
+// let score = "33";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+// let isLoggedIn = 1; //true
+// let isLoggedIn = 0; //false
+ //let isLoggedIn = ""; //false
+ let isLoggedIn = "tushar"; //true
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
