99a88036b39e850abc8d72261d65471d96698728
Tushar Aher <119068251+tusharaher20@users.noreply.github.com>
2023-11-29 04:27:37 +0000

    operations started learning

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,30 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+// more examples
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hi"
+let str2 = "Tushar"
+
+let str3 = str1 + str2
+console.log(str3);
+
+const digit1 = 23
+const digit2 = 44
+
+const digitTotal = digit1 + digit2
+console.log(digitTotal)
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log("1" + 2 + 2 );
