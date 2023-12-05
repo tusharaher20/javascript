b4019ecdce9840d63164f23ebf10fe5867c3e203
Tushar <tusharaher123@outlook.com>
2023-12-05 08:50:30 +0530

    datatypes studied

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -13,5 +13,47 @@
 
 //object
 
-console.log(typeof undefined) //undefined
-console.log(typeof null) //object
+// console.log(typeof undefined) //undefined
+// console.log(typeof null) //object
+
+
+// There are eight basic datatypes in javascript
+
+// 01 VARIABLES
+
+let message = "hello";
+message = 12345;
+
+// a variable in javascript can at one moment be a string and then store a number, such programming languages  that allow such thing, are called dynamically typed.
+
+
+
+//02 NUMBERS
+
+let n = 123;
+n = 34.44;
+// number type represents both integer and floating point numbers.
+
+// special numbers: infinity, -infinity, NaN.
+
+
+//03 STRING 
+
+let str = "Tushar";
+let str22 = "Aher";
+let phrase =  `embedding another string here ${str22}`
+console.log(phrase);
+
+//04 BOOLEAN    
+//true or false
+
+//05 NULL
+
+let age = null;
+//06 UNDEFINED
+
+
+// 07 Objects and Symbols
+
+// The typeof operator
+
