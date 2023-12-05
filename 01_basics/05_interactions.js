b5d2308050952c6a9f14cfd32a3f7c2157ad8181
Tushar <tusharaher123@outlook.com>
2023-12-05 10:10:27 +0530

    interactions in javascript

---
+++ 01_basics/05_interactions.js
@@ -0,0 +1,19 @@
+// Three basic functions
+
+
+
+//ALERT
+
+alert("God friended Me");
+
+//prompt
+// result = prompt(title, [default]);
+
+let yourage = prompt("What is your age ?", 36); //keep empty string in place of 36 for form
+alert(`You are ${yourage} years old!`);
+
+
+//CONFIRM
+//result = confirm(question);
+let confirmation = confirm('Do You Want to continue?');
+//lets you choose a true or false value. yes or no .
